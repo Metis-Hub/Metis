27b021a661676859a32fd4505d654cb9a6c53dd3
Tollpatsch314 <76736177+Tollpatsch314@users.noreply.github.com>
2021-01-07 19:02:51 +0100

    that are teh php-scripts for the index-file

---
+++ index/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		<header>
+		<nav>
+			<a class="active" href="#home">Home</a>
+			<a href="#about">About</a>
+			<a href="#contact">Contact</a>
+			<div class="login-container">
+				<form action="/action_page.php">
+					<input type="text" placeholder="Username" name="username">
+					<input type="password" placeholder="Password" name="psw">
+					<button type="submit">Login</button>
+				</form>
+			</div>
+		</nav>
