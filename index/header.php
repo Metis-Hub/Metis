8b7b0a9237a966c12753b8bf88c193498f5b690a
Tollpatsch314 <76736177+Tollpatsch314@users.noreply.github.com>
2021-01-14 18:15:47 +0100

    Registrierung

--- index/header.php
+++ index/header.php
@@ -3,6 +3,8 @@
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="style.css">
+		<link rel="icon" href="../logoMetis (2).png" type="image/png">
+		<title>Metis</title>
 	</head>
 	<body>
 		<header>
@@ -11,10 +13,11 @@
 			<a href="#about">About</a>
 			<a href="#contact">Contact</a>
 			<div class="login-container">
-				<form action="/action_page.php">
-					<input type="text" placeholder="Username" name="username">
-					<input type="password" placeholder="Password" name="psw">
-					<button type="submit">Login</button>
+				<form action="">
+					<input type="text" placeholder="Username" name="username"/>
+					<input type="password" placeholder="Password" name="psw"/>
+					<button type="button" onclick="window.location.href = '../login/signup.html';">Registrieren</button>
+					<button type="submit" name="login">Login</button>
 				</form>
 			</div>
 		</nav>
