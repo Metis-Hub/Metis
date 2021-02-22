4067b68a52baf834dccbdc5848123d527ef3d128
Tollpatsch314 <Tollpatsch@ANTWORT42>
2021-02-22 14:21:04 +0100

    design verbessert, hintergrundbild einheitlich und fest fixiert

--- registered/tasks/index.php
+++ registered/tasks/index.php
@@ -2,5 +2,33 @@
 	global $position;
 	$position = 1;
 	include("./../header.php");
+?>
+	<div class="table">
+		<center>
+			<table border="1" width="100%" heigth="100%">
+				<tr heigth="10%">
+					<td width="12.5%">Uhrzeit</td>
+					<td width="12.5%">Montag</td>
+					<td width="12.5%">Dienstag</td>
+					<td width="12.5%">Mittwoch</td>
+					<td width="12.5%">Donnerstag</td>
+					<td width="12.5%">Freitag</td>
+					<td width="12.5%">Samstag</td>
+					<td width="12.5%">Sonntag</td>
+				</tr>
+				<tr heigth="90%">
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+			<table>
+		</center>
+	</div>
+<?php
 	include("./../footer.php");
 ?>
