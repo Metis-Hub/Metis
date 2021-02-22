e9e05f6a78f268335e7ded5f31061769ed7c53a0
Tollpatsch314 <Tollpatsch@ANTWORT42>
2021-02-22 21:51:16 +0100

    123

--- registered/grades/index.php
+++ registered/grades/index.php
@@ -3,13 +3,16 @@
 	$position = 1;
 	include("./../header.php");
 
-	if(!isset($_COOKIE["grades_calc"])) {
+	elseif(!isset($_COOKIE["grades_calc"]) && isset($_SESSION["grades_instruction_ok"])) {
 		$_SESSION["grades_first"] = true;
 		header("Location: ./first.php");
 	}
 	else {
-	setcookie("grades_calc", true, time() * 100);
-	}	
+		if(isset($_SESSION["grades_instruction_ok"])) {
+			unset($_SESSION["grades_instruction_ok"]);
+		}
+		setcookie("grades_calc", true, time() * 100);
+	}
 
 ?>
 
