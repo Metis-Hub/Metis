6ac589d65e4efa7b2e3c706b11509c058fa176f7
Tollpatsch314 <Tollpatsch@ANTWORT42>
2021-02-25 10:41:18 +0100

    tmp gelöscht

--- tmpTest.js
+++
@@ -1,7 +0,0 @@
-window.onload = function () {
-    if (window.sessionStorage.getItem("reload") != true)
-    {
-        window.sessionStorage.setItem("reload", true);
-        document.location.reload();
-    }
-}
