9cf374ecd893543451f60a9eb591e6dd31320c94
Tollpatsch314 <Tollpatsch@ANTWORT42>
2021-02-25 10:59:27 +0100

    logo wird nun angezeigt

--- registered/home/index.php
+++ registered/home/index.php
@@ -6,7 +6,7 @@ include("./../header.php");
 	<p>
 		Wilkommen bei Metis, <?php echo $_SESSION["user"]["name"] . "\n";?>
 	</p>
-		<!--Nun für alle deine Daten:
+		<!--Nun für alle, deine erhältlich Daten: *Hu Har har!*
 		< ?php
 			foreach($_SESSION["user"] as $data) {
 				echo "\n<p>\n". $data . "\n</p>\n";
