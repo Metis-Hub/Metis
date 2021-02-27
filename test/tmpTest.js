8a02b942de71144d5ab21bb52ad297ebf34d1371
Tollpatsch314 <Tollpatsch@ANTWORT42>
2021-02-27 18:27:35 +0100

    zeit

---
+++ test/tmpTest.js
@@ -0,0 +1,17 @@
+function UpdateTime() {
+
+	var now = new Date();
+	var hour = now.getHours(), minute = now.getMinutes(), second = now.getSeconds(), day = now.getDay(), date = now.getDate(), month = now.getMonth();
+	var days = ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"];
+
+	document.getElementById("date").innerHTML =
+		days[day] + ", der " +
+		((date < 10)? "0" + date : date) + "." +
+		((month < 10)? "0" + month : month) + "." +
+		now.getFullYear() + "; " +
+		((hour < 10)? "0" + hour : hour) + ":" +
+		((minute < 10)? "0" + minute : minute) + ":" +
+		((second < 10)? "0" + second : second) + "Uhr";
+
+	setTimeout(UpdateTime, 500);
+}
