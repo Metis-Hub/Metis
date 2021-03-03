24f7cb04c923d1b22f9ff251d7730a9e7946db59
Tollpatsch314 <Tollpatsch@ANTWORT42>
2021-03-03 21:58:45 +0100

    update

--- error.php
+++ error.php
@@ -4,7 +4,8 @@
 	<meta charset="UTF-8">
 	<title>Fehler!</title>
 	<link rel="icon" href="./image/faviconMetis.ico" type="image/x-icon" />
-	<?php	
+	<?php
+	$msg = "Fehler am Webserver! Es ist ein Fehler am Webserver aufgetreten.";
 	session_start();
 	if(isset($_SESSION["cookies"]["visual_mode_cookie"]) && $_SESSION["cookies"]["visual_mode_cookie"] == "dark") {
 		echo "<link rel=\"stylesheet\" href=\"./index/style_dark.css\" />\n";
@@ -16,6 +17,40 @@
 	if(!isset($_GET["error"])) {
 		echo "<script type=\"text/JavaScript\">window.history.back();</script>\n";
 	}
+	else {
+		switch($_GET["error"]) {
+			case "400":
+			$msg = "Die Anfrage konnte so nicht verstanden werden%21";
+			break;
+
+			case "401":
+			$msg = "Sie haben keine Berechtigung%2C diesen Zugriff auszuf%FChren%21 Es wurde ein verbotener Zugriff auf dem Webserver ausgef%FChrt.";
+			break;
+
+			case "403":
+			$msg = "Verbotene Anfrage%21 Es wurde eine Verbotene Anfrage an den Webserver gesendet.";
+			break;
+
+			case "500":
+			$msg = "Interner Fehler am Webserver%21 Es ist ein Interner Fehler am Webserver aufgetreten.";
+			break;
+
+			case "503":
+			$msg = "Der Webserver ist zur Zeit %FCberlastet%21 Der Webserver hat wegen %DCberlastung den Dienst %28zeitweise%29 eingestellt%21";
+			break;
+		}
+		$msg2 = "%0A%0AWollen Sie zur%FCck zur Urspr%FCnglichen Seite%3F";
+		echo "\n\t<script type=\"text/JavaScript\">\n";
+		echo "\t\tvar msg = unescape(\"" . $msg . "\");\n";
+		echo "\t\tvar back = confirm(msg + unescape(\"" . $msg2 . "\"));\n";
+		echo "\t\tif(back) {\n";
+		echo "\t\t\twindow.history.back();\n";
+		echo "\t\t}\n";
+		echo "\t\twindow.onload = function() {\n";
+		echo "\t\t\tdocument.getElementById(\"error\").innerHTML = msg + \" - Wir bitten um Entschuldigung.\";\n";
+		echo "\t\t}\n";
+		echo "\t</script>\n";
+	}
 	
 	?>
 </head>
@@ -23,36 +58,10 @@
 <?php
 
 if(isset($_GET["error"])) {
- switch($_GET["error"]) {
-
-  case "400":
-   $meldung = 'Die Anfrage konnte so nicht verstanden werden!';
-   break;
-
-  case "401":
-   $meldung = 'Sie haben keine Berechtigung, diesen Zugriff auszuf&uuml;hren! Es wurde ein verbotener Zugriff auf dem Webserver ausgef&uuml;hrt.';
-   break;
-
-  case "403":
-   $meldung = 'Verbotene Anfrage! Es wurde eine Verbotene Anfrage an den Webserver gesendet.';
-   break;
-
-  case "500":
-   $meldung = 'Interner Fehler am Webserver! Es ist ein Interner Fehler am Webserver aufgetreten.';
-   break;
-
-  case "503":
-   $meldung = 'Der Webserver ist zur Zeit &uuml;berlastet! Der Webserver hat wegen &Uuml;berlastung den Dienst (zeitweise) eingestellt!';
-   break;
-
-  default:
-  $meldung = 'Fehler am Webserver! Es ist ein Fehler am Webserver aufgetreten.';
- }
-
- // Ausgabe
- echo "<p id=\"fehler\">" . $meldung . " &mdash; Wir bitten um Entschuldigung!<br>";
- echo "<a href=\"javascript:history.back();\">Zur&uuml;ck zur vorherigen Seite</a> - ";
- echo "<a href=\"../index.php\">Zur Startseite</a></p>";
+	// Ausgabe
+	echo "<p id=\"error\"></p>";
+	echo "<a href=\"javascript:history.back();\">Zur&uuml;ck zur vorherigen Seite</a> - ";
+	echo "<a href=\"../index.php\">Zur Startseite</a>";
 }
 
 ?>
