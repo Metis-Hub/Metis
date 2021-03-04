8eaf68a8660241d1345c6c8312163615bd5e65f5
Tollpatsch314 <Tollpatsch@ANTWORT42>
2021-03-04 12:49:36 +0100

    update

---
+++ error_404.php
@@ -0,0 +1,29 @@
+<?php
+
+session_start();
+if(!isset($_SESSION["error_404"])) {
+	$_SESSION["error_404"] = true;
+	header("loaction: localhost/Metis/error_404.php");	//
+}
+else {
+	unset($_SESSION["error_404"]);
+}
+
+?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Seite exsistiert nicht!</title>
+		<?php
+			if($_SESSION["cookies"]["visual_mode_cookie"] == "bright") {
+				echo "<link rel=\"stylesheet\" href=\"index/style_dark.css\" />\n";
+			}
+			else {
+				echo "<link rel=\"stylesheet\" href=\"index/style.css\" />\n";
+			}
+		?>
+	</head>
+	<body>
+		
+	</body>
+</html>
