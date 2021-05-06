f1c31d75fde4d01d491332c25995853a88f76fea
Tollpatsch314 <Tollpatsch@ANTWORT42>
2021-05-06 15:18:54 +0200

    update

--- admin/index/index.php
+++ admin/index/index.php
@@ -3,7 +3,12 @@ $position = 0;
 include "../header.php";
 ?>
 		<h1> Admininstation </h1>
-		<p> Hier k&ouml;nnten allgemeine Details stehen </p>
+		<br />
+		<h1> Tipps </h1>
+		<p>
+			<span> F&uuml;r eine sichere Benutzung Empfehlen wir folgendes: </span>
+			<li>  </li>
+		</p>
 <?php
 include "../footer.php";
 ?>
