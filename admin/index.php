275ee1aaf1ad70c9413f1ad98959fd402e5b6e6c
Tollpatsch314 <Tollpatsch@ANTWORT42>
2021-05-09 15:00:30 +0200

    update

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,3 @@
 <?php
-header("Location: index/index.php");
+header("location: index/");
 ?>
--- admin/index/index.php
+++ admin/index/index.php
@@ -7,7 +7,8 @@ include "../header.php";
 		<h1> Tipps </h1>
 		<p>
 			<span> F&uuml;r eine sichere Benutzung Empfehlen wir folgendes: </span>
-			<li>  </li>
+			<li> Ein HTTPS-Zertifikat </li>
+			<li> Sichere Passw&ouml;rter </li>
 		</p>
 <?php
 include "../footer.php";
