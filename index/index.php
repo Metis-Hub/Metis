488fe9e6642672eecc24ccd9aa0ab520215ed188
Jiscona-private <jakob.paridon@gmail.com>
2021-05-26 20:11:24 +0200

    Änderung des Home-Textes

--- index/index.php
+++ index/index.php
@@ -14,49 +14,36 @@ elseif(!isset($_SESSION["cookies"]["request_send"]) || $_SESSION["cookies"]["req
 include("header.inc.php");
 
 ?>
+
 	<center>
 		<div>
 			
 			<br />
 			<div>
-				<h1>
-					Hallo und Herzlich Willkommen bei Metis!
-				</h1>
+				<h1 style="font-size: 2cm;">
+					Metis - school made easy.
+				</h1>				
 			</div>
 
 			<br />
 
 			<div>
 				<p>
-					<h3>Was ist Metis?</h3>
+					<h2>Metis‘ Aufgabe lässt sich an unserem Motto gut erkennen: wir wollen Schule einfach machen.</h2>
 				</p>
-				<p>
-					Metis, dies ist eine Schulplattform, welche Aufgabenplanung und Optimierung f&uuml;r Sie &uuml;bernimmt.
-					<br />
-					Zudem erm&ouml;glichen wir einen Stunden- und Vertretungsplaner in dem die Sch&uuml;ler ihre Aufgaben ansehen und l&ouml;sen k&ouml;nnen.
-					Wir bieten den Lehrern die M&ouml;glichkeit, Aufgaben hier f&uuml;r die Sch&uuml;ler hochzuladen.
+				<p style="font-size: 0.5cm;">
+				Für dich – für euch – für alle. Dafür löst Metis all die Probleme, die den Schulalltag so anstrengend machen:
+				<br/>
+				kaum Organisation, unordentliche Aufgabenplanung, fehlende Kommunikation, Lernen für Tests und Arbeiten, … 
+				<br/>
+				Löst das Kopfschmerzen aus? Dann ist Metis der perfekte Service für dich! Hier kannst du deine Aufgaben verwalten, 
+				<br/>
+				mit Lehrern und Schülern kommunizieren und außerdem individuell Vokabeln, Rechnen und vieles mehr lernen und üben.
 				</p>
-				<p>
-					Metis &uuml;berzeugt zudem mit den M&ouml;glichkeiten des Lernens, wie zum Beispiel	&uuml;ber Quizze, Vokabeltrainer und anderen Eastereggs.
-				</p>
-			</div>
 
-			<div>
-				<p>
-					<h3>Wie kann ich Metis nutzen?</h3>
+				<p style="font-size: 0.5cm;">
+				Melde dich jetzt oben links an und fang an, Schule endlich zu genießen!
 				</p>
-				<p>
-					Nutzt deine Schule immer noch nicht Metis?
-					<br />
-					Frag einfach deinen Klassenleiter, und zeige Ihm Metis. Metis muss n&auml;hmlich von der Schule aktiviert werden.
-					<br />
-					Ist dies getan, bekommst du dein Benutztername, und Passwort welches du bei der Ersten Anmeldung aufgefordert wirst zu &Auml;ndern.
-					<br />
-					<I>Und wage es ja nicht 123, passwort oder &auml;hnliches zu verwenden!</I> &#128544;
-				</p>
-			</div>
-			<div>
-				<a target="_blank" href="https://github.com/Metis-Git/Metis"> <i class="fa fa-github fa-5x"></i> </a>
 			</div>
 		</div>
 	</center>
