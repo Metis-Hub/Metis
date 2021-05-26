60741cfa6aadceb3b615b367719c8f0c318903e0
Tollpatsch314 <Tollpatsch@ANTWORT42>
2021-05-26 17:53:13 +0200

    update

--- error_404.php
+++ error_404.php
@@ -8,28 +8,6 @@ $link = "http://" . $_SERVER["HTTP_HOST"] . "/Metis/error_404.php";
 <html>
 	<head>
 		<title>Seite exsistiert nicht!</title>
-		<?php /*
-			if($_SESSION["cookies"]["visual_mode_cookie"] == "dark") {	//Style wird nicht gewählt
-				echo "<link rel=\"stylesheet\" href=\"./index/style_dark.css\" />\n";
-			}
-			else {
-				echo "<link rel=\"stylesheet\" href=\"./index/style.css\" />\n";
-			}
-
-			if(!isset($_SESSION["error_404"])) {
-				$_SESSION["error_404"] = true;
-				echo "\t\t\t<script type=\"text/JavaScript\">\n";
-				echo "\t\t\t\twindow.onload = function () {\n";
-				echo "\t\t\t\twindow.location.href = \"" . $link . "\"\n";
-				echo "\t\t\t}\n";
-				echo "\t\t</script>\n";
-			}
-			else {
-				unset($_SESSION["error_404"]);
-				echo $_SERVER['PHP_SELF'];
-				echo $_SERVER['HTTP_REFERER'];
-			}*/
-		?>
 	</head>
 	<body>
 		
