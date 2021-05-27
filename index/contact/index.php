0d6dd7c1ac454df33a970eec8cfeebc7a4bbf46f
Tollpatsch314 <Tollpatsch@ANTWORT42>
2021-05-27 15:28:04 +0200

    update

--- index/contact/index.php
+++ index/contact/index.php
@@ -8,7 +8,7 @@ include("./../header2.inc.php");
 			<br />
 			<div>
 				<h1>
-					Impressum - Was wei&szlig; ich . . . 
+					Impressum - Metis 
 				</h1>
 			</div>
 
@@ -16,13 +16,14 @@ include("./../header2.inc.php");
 
 			<div>
 				<p>
-					<h3>Cookies</h3>
+					Metis ist ein im Rahmen des Kurses &quot;Informatik 2&quot; am Sch&uuml;lerrechenzentrum Dresden entstandenen Projekt. F&uuml;r die Inhalte dieser Website sind Bruno Hoffmann,
+					<br />
+					Karl Jahn und Jakob Paridon verantwortlich. Die Kursleitung erfolgte durch Herrn Dr. Unger sowie durch Herrn Stock. Die alleinige rechtliche Verantwortung sowie jedwede Haftung &uuml;bernimmt Bruno Hoffmann.
 				</p>
 				<p>
-					Irgendwas
-				</p>
-				<p>
-					BlaBlaBlub . . .. . . . .. . .. . . .. . .. .. . .. . .
+					Metis benutzt Kekse, welche die Funktion der Website gew&auml;hrleisten. Diese Kekse sind essentiell und m&uuml;ssen aktiviert sein. Es werden keine unn&ouml;tigen Daten gesammelt.
+					<br />
+					Die auf den Keksen gespeicherten Daten werden nicht an Dritte weitergegeben.
 				</p>
 			</div>
 		</div>
