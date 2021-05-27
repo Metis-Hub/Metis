c2f9d8703e2c165287dcb10e2a60ded00c81415d
Tollpatsch314 <Tollpatsch@ANTWORT42>
2021-05-27 16:53:33 +0200

    update

--- index/contact/index.php
+++ index/contact/index.php
@@ -18,7 +18,7 @@ include("./../header2.inc.php");
 				<p>
 					Metis ist ein im Rahmen des Kurses &quot;Informatik 2&quot; am Sch&uuml;lerrechenzentrum Dresden entstandenen Projekt. F&uuml;r die Inhalte dieser Website sind Bruno Hoffmann,
 					<br />
-					Karl Jahn und Jakob Paridon verantwortlich. Die Kursleitung erfolgte durch Herrn Dr. Unger sowie durch Herrn Stock. Die alleinige rechtliche Verantwortung sowie jedwede Haftung &uuml;bernimmt Bruno Hoffmann. ;)
+					Karl Jahn und Jakob Paridon verantwortlich. Die Kursleitung erfolgte durch Herrn Dr. Unger sowie durch Herrn Stock. <!--Die alleinige rechtliche Verantwortung sowie jedwede Haftung &uuml;bernimmt Bruno Hoffmann. ;)-->
 				</p>
 				<p>
 					Metis benutzt Kekse, welche die Funktion der Website gew&auml;hrleisten. Diese Kekse sind essentiell und m&uuml;ssen aktiviert sein. Es werden keine unn&ouml;tigen Daten gesammelt.
