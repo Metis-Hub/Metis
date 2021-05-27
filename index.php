3dab7383d96470d78a7ae719960ab9386866abb6
Tollpatsch314 <Tollpatsch@ANTWORT42>
2021-05-27 16:10:22 +0200

    update

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
 session_start();
 if(!isset($_SESSION["FIRST_SESSION"])) header("location: index_first.php");
-else {
-	unset($_SESSION["FIRST_SESSION"]);
-	unlink("index_first.php");
-	header("location: admin/");
-}
+exit;
+unset($_SESSION["FIRST_SESSION"]);
+unlink("index_first.php");
+header("location: admin/index/");
 ?>
